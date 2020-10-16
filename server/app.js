99cff25dcd322cbc9b2a36acca015c2c4329aaaa
Eas <me@eas.web.id>
2020-10-16 18:24:28 +0700

    Fix conflict: app.js

--- server/app.js
+++ server/app.js
@@ -4,15 +4,14 @@ const app = express()
 const port = process.env.PORT || 3000
 const route = require('./routes')
 const cors = require('cors')
+const errHandler = require('./middleware/errorHandler')
 
-app.use(cors())
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
-app.use(route)
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`)
-// })
-
-module.exports = app
+app
+  .use(cors())
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
+  .use(route)
+  .use(errHandler)
+  .listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
