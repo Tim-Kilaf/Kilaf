0dd29a133e4153bf0c053767332d73997635d190
ArdyAF <ardy.fauzan@yahoo.co.id>
2020-10-17 07:49:13 +0700

    feat: F-B Add Create Item

--- client/src/store/actions/actionsItem.js
+++ client/src/store/actions/actionsItem.js
@@ -2,6 +2,9 @@ export function createItem(payload) {
   return dispatch => {
     fetch('http://localhost:3001/item/create', {
       method: 'POST',
+      headers: {
+        access_token: localStorage.getItem('access_token')
+      },
       body: payload
     })
       .then(res => res.json())
--- server/controllers/ItemController.js
+++ server/controllers/ItemController.js
@@ -1,4 +1,4 @@
-const { Item, ItemPicture } = require('../models')
+const { Items, ItemPictures } = require('../models')
 const path = require('path')
 
 class ItemController {
@@ -8,13 +8,27 @@ class ItemController {
 
   static async createItem(req, res, next) {
     try {      
+      delete req.body.image
+      const payload = {
+        ...req.body,
+        current_price: req.body.starting_price,
+        status: 'unsold',
+        UserId: req.user.id
+      }            
+    
+      const item = await Items.create(payload)  
       const images = req.files.images
-      
+
       for(let i = 0; i < images.length;i++) {
+        await ItemPictures.create({
+          ItemId: item.id,
+          path: `/assets/images/${images[i].name}`
+        })
         images[i].mv(path.join(__dirname, `../../client/src/assets/images/${images[i].name}`))
-      }
-      res.status(201).json({msg: req.files})
+      }      
+      res.status(201).json({message: 'Sucessfully Created'})
     } catch(errors) {
+      console.log(errors)
       next(errors)
     }
   }
