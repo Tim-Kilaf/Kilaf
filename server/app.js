e018061d633b91b09c3d17a9f8caabe98fe32f90
Eas <me@eas.web.id>
2020-10-18 13:18:23 +0700

    add socket.io on bootstrap

--- server/app.js
+++ server/app.js
@@ -1,18 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const http = require('http')
 const port = process.env.PORT || 3000
 const route = require('./routes')
 const {errorHandler} = require('./middleware/errorHandler')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
+const io = require('socket.io')(http.createServer(app))
+
+// const socket = require('../config/socket')(io)
 
 app
-.use(cors())
-.use(fileUpload())
-.use(express.urlencoded({ extended: false }))
-.use(express.json())
-.use(route)
-.use(errorHandler)
+    .use(cors())
+    .use(fileUpload())
+    .use(express.urlencoded({ extended: false }))
+    .use(express.json())
+    .use(route(io))
+    .use(errorHandler)
 
 module.exports = app
