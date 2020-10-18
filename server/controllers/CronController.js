740624cae9f3499a1b5e9614d4d61ff4ba26db5d
rizkygustianto <rizkyrebellion@gmail.com>
2020-10-18 14:52:23 +0700

    CRON Function (Resolve Bids to Transaction)
    feat: auto resolve winning bids to transaction every minute

--- server/controllers/CronController.js
+++ server/controllers/CronController.js
@@ -1,4 +1,5 @@
 const { Items, Biddings } = require('../models')
+const {Op} = require('sequelize')
 
 class CronController {
     static getItemByEndDate = async (req,res,next) => {
@@ -15,9 +16,32 @@ class CronController {
 
             console.log(UserId, ItemId, price)
 
-            return res.status(200).json(itemTimeOut)
+            // return res.status(200).json(itemTimeOut)
+            return itemTimeOut
         } catch (error) {
             console.log(error)
+        }
+    }
+    static getWinningBids = async() => {
+        try {
+            const itemTimeOut = await Items.findAll({
+                // include: [Biddings],
+                where: {
+                    end_date: {
+                        [Op.lte]: new Date() 
+                    },
+                    status: 'unsold'
+                },
+                // order: [[ Biddings, 'price', 'DESC' ]]
+            })
+            // const highestBidder = itemTimeOut[0].Biddings[0]
+            // const {UserId, ItemId, price} = highestBidder
+
+            // console.log(UserId, ItemId, price)
+            console.log({itemTimeOut}, 'contorller');
+            return {itemTimeOut}.itemTimeOut
+        } catch (error) {
+            console.log(error);
         }
     }
 }
