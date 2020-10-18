c896c21bc95b949f0ad502c50f6b1186974f8516
rizkygustianto <rizkyrebellion@gmail.com>
2020-10-18 19:27:35 +0700

    Fix errors on buyout endpoint

--- server/routes/endpoints/transaction.js
+++ server/routes/endpoints/transaction.js
@@ -1,6 +1,6 @@
 module.exports = (io) => {
     const TransactionController = require('../../controllers/TransactionControllers')(io)
-const TransactionController = require('../../controllers/TransactionControllers')
+// const TransactionController = require('../../controllers/TransactionControllers')
 const authenticate = require('../../middleware/authentication')
 
 const transactionEndpoints = require('express').Router()
@@ -9,7 +9,7 @@ const transactionEndpoints = require('express').Router()
     .get('/', TransactionController.read)
     .get('/:UserId', TransactionController.getUserTransactions)
     .post('/create/:ItemId', TransactionController.create)
-    .get('/buyout/:ItemId', authenticate, TransactionController.createForBuyout)
+    // .get('/buyout/:ItemId', authenticate, TransactionController.createForBuyout)
     .delete('/:UserId', TransactionController.delete)
     .put('/paid/:UserId', TransactionController.paid)
 
