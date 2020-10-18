a12e92735e10067c992d9e994a5a1437c0e2e0f2
rizkygustianto <rizkyrebellion@gmail.com>
2020-10-18 19:16:36 +0700

    commit to merge buyout

--- server/routes/endpoints/cron.js
+++
@@ -1,7 +0,0 @@
-const cronEndpoints = require('express').Router()
-const CronController = require('../../controllers/CronController')
-
-cronEndpoints
-    .get('/', CronController.getItemByEndDate)
-
-module.exports = cronEndpoints
