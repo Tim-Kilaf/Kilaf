ac202fce768339ce3af21288974dba35a94ad8fc
FaudzanAdzima <faudzanad@gmail.com>
2020-10-19 22:20:29 +0700

    feat: payment gateway Stripe

--- server/controllers/PaymentController.js
+++ server/controllers/PaymentController.js
@@ -1,41 +1,60 @@
 const { Payments } = require('../models')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const { v4: uuidv4 } = require('uuid')
 
 class PaymentController {
     constructor(io) {
         this.io = io
     }
-    stripe (req,res,next) {
-        console.log('masuk stripe')
-        const { product, token } = req.body
+    stripe = async (req,res,next) => {
+     
+        const { price, token } = req.body
+
+        if(req.user.email === token.email){
+           
+            const idempotencyKey = uuidv4()
+         
+            stripe.customers.create({
+                email: token.email,
+                source: token.id
+            })
+            .then((customer) => {
+                console.log(customer)
+                return stripe.invoiceItems.create({
+                    customer: customer.id,
+                    amount: price,
+                    currency: 'usd'
+                },{idempotencyKey})
+            })
+            .then((invoiceItem) => {
+                console.log(invoiceItem)
+                return stripe.invoices.create({
+                    collection_method: 'send_invoice',
+                    customer: invoiceItem.customer,
+                    due_date: new Date
+                })
+            })
+            .then((invoice) => {
+                return res.status(200).json(invoice)
+            })
+            .catch(error => {
+                console.error(error)
+                next(error)
+            });
+        }else{
+            let error = {
+                code: 400,
+                message: 'Invalid email'
+            }
+            next(error)
+        }
         
-        stripe.customers.create({
-            email: 'customer@example.com',
-            source: '1'
-        })
-        .then(customer => {
-            console.log(customer.id)
-            return stripe.invoiceItems.create({
-                customer: token.name,
-                amount: token.price,
-                currency: 'usd'
-            })
-        })
-        .then((invoiceItem) => {
-            return stripe.invoices.create({
-                collection_method: 'send_invoice',
-                customer: invoiceItem.customer,  
-            })
-        })
-        .then((invoice) => {
-            return res.json(invoice)
-        })
-        .catch(error => console.error(error));
     }
     read = async (req, res, next) => {
+        console.log('masuk read')
         try {
             const payment = await Payments.findAll()
-            return res.sttaus(200).json(payment)
+            return res.status(200).json(payment)
         } catch (err) {
             console.log(err)
             return next(err)
--- server/routes/endpoints/payment.js
+++ server/routes/endpoints/payment.js
@@ -1,8 +1,11 @@
+const authenticate = require('../../middleware/authentication')
+
 module.exports = (io) => {
     const PaymentController = require('../../controllers/PaymentController')(io)
 const paymentEndpoint = require('express').Router()
 
     return paymentEndpoint
     .get('/', PaymentController.read)
+    .post('/', authenticate, PaymentController.stripe)
 }
 // module.exports = paymentEndpoint
