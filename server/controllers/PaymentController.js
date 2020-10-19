dd0d3532af5186daa6be45124ce8ced261724438
FaudzanAdzima <faudzanad@gmail.com>
2020-10-19 16:48:07 +0700

    implement stripe

--- server/controllers/PaymentController.js
+++ server/controllers/PaymentController.js
@@ -1,8 +1,36 @@
 const { Payments } = require('../models')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 class PaymentController {
     constructor(io) {
         this.io = io
+    }
+    stripe (req,res,next) {
+        console.log('masuk stripe')
+        const { product, token } = req.body
+        
+        stripe.customers.create({
+            email: 'customer@example.com',
+            source: '1'
+        })
+        .then(customer => {
+            console.log(customer.id)
+            return stripe.invoiceItems.create({
+                customer: token.name,
+                amount: token.price,
+                currency: 'usd'
+            })
+        })
+        .then((invoiceItem) => {
+            return stripe.invoices.create({
+                collection_method: 'send_invoice',
+                customer: invoiceItem.customer,  
+            })
+        })
+        .then((invoice) => {
+            return res.json(invoice)
+        })
+        .catch(error => console.error(error));
     }
     read = async (req, res, next) => {
         try {
