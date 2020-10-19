8afd16c0dab4329cb3fb983517000b3d5c138e23
ArdyAF <ardy.fauzan@yahoo.co.id>
2020-10-19 22:46:50 +0700

    F-Style Dashboard

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -4,14 +4,20 @@ import BiddingCard from '../../components/cards/BiddingCard'
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux'
 import { getItems } from '../../store/actions/actionsItem'
+import { DashboadCarousel } from '../../components/carousel/DashboadCarousel'
 import { disconnectSocket, initiateSocket, subscribeToDashboard } from '../../sockets/itemSocket';
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
+  mainContainer: {
     width: '70%',
     margin: '0 auto',
+    marginTop: 40,
+    marginBottom: 40
+  },
+  container: {        
+    padding: 14,
+    borderRadius: 15, 
+    marginTop: 30,   
     [theme.breakpoints.down('xs')]: {
       width: '90%',
     },
@@ -41,13 +47,12 @@ export default function Dashboard() {
   }, [dispatch])
 
   return (
-    <Box>
-      <Box className={classes.container}>
-        {items.length > 0 && items.map(data => {
-          return (
-            <BiddingCard data={data} />
-          )
-        })}
+    <Box class={classes.mainContainer}>
+      <Box className={classes.container} boxShadow={3} >
+        <DashboadCarousel data={items} text="Latest Bids"/>
+      </Box>
+      <Box className={classes.container} boxShadow={3}>
+        <DashboadCarousel data={items} text="Hottest Bids"/>
       </Box>
     </Box>
   )
