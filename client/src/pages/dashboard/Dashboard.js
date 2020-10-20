28757540014e8931bf4ca330e3953618e910fae0
ArdyAF <ardy.fauzan@yahoo.co.id>
2020-10-20 13:03:13 +0700

    Fix Import Error

--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Box from '@material-ui/core/Box';
-import BiddingCard from '../../components/cards/BiddingCard'
+import { DashboadCarousel } from '../../components/carousel/DashboadCarousel'
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux'
 import { getItems } from '../../store/actions/actionsItem'
