06ae7980ca2896b34621a5c2eb4e0f031129ed92
rizkygustianto <rizkyrebellion@gmail.com>
2020-10-21 16:13:25 +0700

    Updated Login/Register
    feat: login/register added logo and navigation to login/register

--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -5,30 +5,36 @@ import {
   Paper, 
   Typography, 
   TextField,
-  Button,
+  Button
 } from "@material-ui/core";
 import { useDispatch } from 'react-redux';
 import { register } from '../../store/actions/actionsUser'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
+import Login from '../login/Login'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
     flexwrap: 'wrap',
     height: '100vh',
-    '& > *': {
-      // margin: theme.spacing(),
-      width: theme.spacing(70),
-      height: theme.spacing(60),
-    }
+    // '& > *': {
+    //   // margin: theme.spacing(),
+    //   width: theme.spacing(70),
+    //   height: theme.spacing(60),
+    // }
   },
   form: {
     '& > *': {
       margin: 15,
       width: '92%',
     },
+  },
+  image: {
+    marginBottom: 75,
+    width: '20%'
   }
 }))
 
@@ -66,8 +72,14 @@ export default function Register() {
     history.push('/login')
   }
 
+  const navigateLogin = (event) => {
+    event.preventDefault()
+    history.push('/login')
+  }
+
   return (
     <Container className={classes.root}>
+      <img src="https://i.imgur.com/DkXvWFJ.png" alt="Kilaf" className={classes.image} />
       <Paper elevation={3}>
         <Typography style={{margin: 20}} variant="h3">
           Register
@@ -111,9 +123,9 @@ export default function Register() {
             Register
           </Button>
         </form>
-        <Typography style={{display: 'flex', justifyContent: 'center'}}>
+        <Link to='/login' style={{display: 'flex', justifyContent: 'center', paddingBottom: '20px'}}>
           Have an account? Login here
-        </Typography>
+        </Link>
       </Paper>
     </Container>
   )
